c4843a1f118ffc678ccae727386e342a41a837d8
vvveracruz <login@vgg.cz>
2020-11-17 11:14:12 +0000

    working through building with components tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,10 @@
 import React from "react"
 
 export default function Home() {
-  return <div><h1>Hello world!</h1></div>
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <p>How exciting.</p>
+    </div>
+  );
 }
